eb2a62591e10fc7925d0f9da65e8b981cfad938b
cmclark00 <cmclark00@gmail.com>
2022-10-24 13:15:48 -0400

    Can unrar a given file and move

--- extract.py
+++ extract.py
@@ -1,3 +1,15 @@
+import os
 import pip
 import patoolib
-patoolib.extract_archive("/home/corey/Downloads/CC.rar", outdir="home/corey/Documents")
+
+Castle_Crashers = ("Castle_Crashers")
+Check_Folder= os.path.isdir(Castle_Crashers)
+
+if not Check_Folder:
+    os.makedirs(Castle_Crashers)
+    print("Created Folder : ",Castle_Crashers)
+
+else:
+    print(Castle_Crashers, "Folder already exists.")
+
+patoolib.extract_archive("/home/corey/Downloads/CC.rar", outdir="/home/corey/Documents/Castle_Crashers")
