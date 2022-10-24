8213d8dc6d6b9070386e59accd434ed6be5b7b9a
cmclark00 <cmclark00@gmail.com>
2022-10-24 17:39:28 -0400

    This should scan a given folder for a file then unpacl that file to a given folder with same name

--- extract.py
+++ extract.py
@@ -1,15 +1,20 @@
+
+from ast import Str
 import os
 import pip
 import patoolib
 
-Castle_Crashers = ("Castle_Crashers")
-Check_Folder= os.path.isdir(Castle_Crashers)
+FileName = os.listdir('/home/corey/Downloads')
+FileName = (str(FileName).replace('[','').replace(']','').replace("'",''))
+  
+MyDir = (FileName)
+Check_Folder= os.path.isdir(str(FileName))
 
 if not Check_Folder:
-    os.makedirs(Castle_Crashers)
-    print("Created Folder : ",Castle_Crashers)
+    os.makedirs(str(FileName))
+    print("Created Folder : ", FileName)
 
 else:
-    print(Castle_Crashers, "Folder already exists.")
+    print(FileName, "Folder already exists.")
 
-patoolib.extract_archive("/home/corey/Downloads/CC.rar", outdir="/home/corey/Documents/Castle_Crashers")
+patoolib.extract_archive("/home/corey/Downloads/" + str(FileName), outdir="/home/corey/Documents/" + str(FileName))
