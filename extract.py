f9c20abe83fe44ef0d82e13cff06eeb4e47a347f
cmclark00 <cmclark00@gmail.com>
2022-10-24 18:40:23 -0400

    The program will now take a single rar file in /home/username/XBLA and unpack to home/username/XBLA_Unpacked

--- extract.py
+++ extract.py
@@ -1,20 +1,23 @@
 
 from ast import Str
 import os
+import os.path
 import pip
 import patoolib
-
-FileName = os.listdir('/home/corey/Downloads')
+#Changes ~ to /home/usernam
+HomePath = os.path.expanduser('~')
+#this sets FileName to the name of the file in /home/username/XBLA as a string and removes the [] and ''
+FileName = os.listdir(HomePath + '/XBLA/')
 FileName = (str(FileName).replace('[','').replace(']','').replace("'",''))
-  
+#This should check if there is already the same named directory in XBLA_Unpacked folder and if not will create it
 MyDir = (FileName)
-Check_Folder= os.path.isdir(str(FileName))
+Check_Folder= os.path.isdir(str(HomePath + '/XBLA_Unpacked/' + FileName))
 
 if not Check_Folder:
-    os.makedirs(str(FileName))
+    os.makedirs(str(HomePath + '/XBLA_Unpacked/' + FileName))
     print("Created Folder : ", FileName)
 
 else:
     print(FileName, "Folder already exists.")
-
-patoolib.extract_archive("/home/corey/Downloads/" + str(FileName), outdir="/home/corey/Documents/" + str(FileName))
+#This extracts the rar in the XBLA folder to XBLA_Unpacked
+patoolib.extract_archive(HomePath + "/XBLA/" + (FileName), outdir= HomePath + "/XBLA_Unpacked/" + (FileName))
