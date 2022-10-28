adbac985199910afcc641ea6591b78596a800b65
John Ciubuc <john.ciubuc@ttuhsc.edu>
2022-10-27 19:23:59 -0500

    typo fixes on extract.py

--- extract.py
+++ extract.py
@@ -53,55 +53,57 @@ for i in range(len(AllDirs)):
             shutil.rmtree(dst_folder)
 
         # move source to dest
-        os.rename(src_folder, dst_folder)
+        try:
+            os.rename(src_folder, dst_folder)
+        except:
+            print(f'Unable to move {src_folder} to {dst_folder}')
 
-# This grabs the new correct name and set FileName to that
-    FileName = (str(FileName).replace(
-        '[', '').replace(']', '').replace("'", ''))
+    # This grabs the new correct name and set FileName to that
+        FileName = (str(FileName).replace(
+            '[', '').replace(']', '').replace("'", ''))
 
-  # This sets FileName to correct name with a / for the path and then grabs the next directory name, while also creating the level structure for paths
-    FileName = FileName + '/'
-    Levels = []
-    Levels = [HomePath + UnpackedPath + FileName]
-    Levels = [x + FileName * ii for x in Lists for ii in range(4)]
+      # This sets FileName to correct name with a / for the path and then grabs the next directory name, while also creating the level structure for paths
+        FileName = FileName + '/'
+        Levels = [HomePath + UnpackedPath + FileName]
+        Levels = [x + FileName * ii for x in Lists for ii in range(4)]
 
-    # This changes the 2nd subdirectory to the correct name
-    renameLevelDir(Lists.at(1))
-    # This changes the 3rd subdirectory to the correct name
-    renameLevelDir(Lists.at(2))
+        # This changes the 2nd subdirectory to the correct name
+        renameLevelDir(Lists.at(1))
+        # This changes the 3rd subdirectory to the correct name
+        renameLevelDir(Lists.at(2))
 
-# This launches wxPirs and tells the user to run it on the correct file then close it
-    input("Press Enter to launch wxPirs. Once launched, select the innermost file of " +
-          FileName[:-1] + " and close wxPirs")
-    run("wxpirs.exe")
+    # This launches wxPirs and tells the user to run it on the correct file then close it
+        input("Press Enter to launch wxPirs. Once launched, select the innermost file of " +
+              FileName[:-1] + " and close wxPirs")
+        run("wxpirs.exe")
 
-# This renames default.xex to the correct FileName.xex
-    for filename in DirectoryList:
-        filename = "default.xex"
-        src = filename
-        dst = FileName[:-1] + ".xex"
-        os.rename(os.path.join(Lists.at(3), src), os.path.join(Lists.at(3), dst))
+    # This renames default.xex to the correct FileName.xex
+        for filename in DirectoryList:
+            filename = "default.xex"
+            src = filename
+            dst = FileName[:-1] + ".xex"
+            os.rename(os.path.join(Lists.at(3), src), os.path.join(Lists.at(3), dst))
 
-# This moves everything to the top level directory
-        destination = LevelOne
-        AllFiles = os.listdir(Lists.at(3))
-        for f in AllFiles:
-            src = os.path.join(Lists.at(3), f)
-            dst = os.path.join(destination, f)
-            os.rename(src, dst)
+    # This moves everything to the top level directory
+            destination = Lists.at(0)
+            AllFiles = os.listdir(Lists.at(3))
+            for f in AllFiles:
+                src = os.path.join(Lists.at(3), f)
+                dst = os.path.join(destination, f)
+                os.rename(src, dst)
 
-# This asks the user if they want to delete the original Rar files and does so if they choose Y for yes
-        if i == len(AllDirs) - 1:
-            DeletePath = HomePath + PackedPath
-            Delete = input(
-                "Would you like the original Rar files to be deleted? Enter Y for yes, or N for no: ")
-            if Delete == "Y":
-                shutil.rmtree(HomePath + "/XBLA")
-                os.makedirs(HomePath + "/XBLA")
-                print("Files deleted. The program has finished running.")
-            else:
-                quit("The program has finished running.")
+    # This asks the user if they want to delete the original Rar files and does so if they choose Y for yes
+            if i == len(AllDirs) - 1:
+                DeletePath = HomePath + PackedPath
+                Delete = input(
+                    "Would you like the original Rar files to be deleted? Enter Y for yes, or N for no: ")
+                if Delete == "Y":
+                    shutil.rmtree(HomePath + "/XBLA")
+                    os.makedirs(HomePath + "/XBLA")
+                    print("Files deleted. The program has finished running.")
+                else:
+                    quit("The program has finished running.")
 
-# This deletes all of the extra subdirectories inside the game directories
-        for xx in range(1,4):
-            shutil.rmtree(Lists.at(xx))
+    # This deletes all of the extra subdirectories inside the game directories
+            for xx in range(1,4):
+                shutil.rmtree(Lists.at(xx))
