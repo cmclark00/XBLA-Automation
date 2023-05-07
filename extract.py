3eda1b553b3c5983eebcaeed982e9b1eea84c811
cmclark00 <116466213+cmclark00@users.noreply.github.com>
2023-05-07 00:27:55 -0400

    Update extract.py
    Oh we fancy! Now with accurate progress bar!

--- extract.py
+++ extract.py
@@ -1,6 +1,8 @@
 import os
 import patoolib
 import shutil
+from alive_progress import alive_bar
+import time
 
 # Get the user's home directory
 home_dir = os.path.expanduser("~")
@@ -16,39 +18,41 @@ if not os.path.exists(xbla_unpacked_dir):
     os.mkdir(xbla_unpacked_dir)
 
 # Iterate over all of the RAR archives in the XBLA folder
-for archive in os.listdir(xbla_dir):
-    if archive.endswith(".rar"):
-        if not os.path.exists(os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_"))):
-            os.mkdir(os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_")))
+with alive_bar(len(os.listdir(xbla_dir))) as bar:
+    for archive in os.listdir(xbla_dir):
+        if archive.endswith(".rar"):
+            if not os.path.exists(os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_"))):
+                os.mkdir(os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_")))
 
-        # Unpack the archive to the XBLA_Unpacked folder
-        patoolib.extract_archive(os.path.join(xbla_dir, archive),
-                                 outdir = (os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_"))))
+            # Unpack the archive to the XBLA_Unpacked folder
+            patoolib.extract_archive(os.path.join(xbla_dir, archive),
+                                        outdir = os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_")), verbosity=-1)
 
-        # Get the path to the innermost subdirectory
-        for subdir, dirs, files in os.walk(xbla_unpacked_dir):
-            for file in files:
-    # Get the path to the innermost file in each subdirectory
-                innermost_file = os.path.join(subdir, file)
+            # Get the path to the innermost subdirectory
+            for subdir, dirs, files in os.walk(xbla_unpacked_dir):
+                for file in files:
+        # Get the path to the innermost file in each subdirectory
+                    innermost_file = os.path.join(subdir, file)
         
 
-        # Get the name of the extensionless file in the innermost subdirectory
-        extensionless_file = innermost_file
+            # Get the name of the extensionless file in the innermost subdirectory
+            extensionless_file = innermost_file
 
-        # Rename the extensionless file to match the name of the archive it came from
-        os.rename(os.path.join(extensionless_file), os.path.join(xbla_unpacked_dir, archive.replace(".rar", ".pirs")))
+            # Rename the extensionless file to match the name of the archive it came from
+            os.rename(os.path.join(extensionless_file), os.path.join(xbla_unpacked_dir, archive.replace(".rar", ".pirs")))
 
-        # Delete the innermost directory
-        shutil.rmtree(os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_")))
+            # Delete the innermost directory
+            shutil.rmtree(os.path.join(xbla_unpacked_dir, archive[:-4].replace(" ", "_")))
         
-        #Remove the .pirs extension
-        for subdir, dirs, files in os.walk(xbla_unpacked_dir):
-            for file in files:
-                src = file
-                dst = file.replace(".pirs", "")
-                os.rename(os.path.join(xbla_unpacked_dir, src), (os.path.join(xbla_unpacked_dir, dst)))
+            #Remove the .pirs extension
+            for subdir, dirs, files in os.walk(xbla_unpacked_dir):
+                for file in files:
+                    src = file
+                    dst = file.replace(".pirs", "")
+                    os.rename(os.path.join(xbla_unpacked_dir, src), (os.path.join(xbla_unpacked_dir, dst)))
             
-
+        print(file)
+        bar()
 # Print a message to let the user know that the operation was successful
 print("Unpacking complete!")
 
